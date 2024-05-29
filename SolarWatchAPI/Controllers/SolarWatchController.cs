2dff668aa6a9a4a331f61b15d70987577ccc6e62
ndrs06 <szaki1006@gmail.com>
2024-05-29 11:14:51 +0200

    :construction: feat: created SolarWatchController.cs with a Get endpoint

---
+++ SolarWatchAPI/Controllers/SolarWatchController.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Mvc;
+using SolarWatchAPI.Models;
+
+namespace SolarWatchAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+
+public class SolarWatchController : ControllerBase
+{
+    private readonly ILogger<SolarWatchController> _logger;
+
+    public SolarWatchController(ILogger<SolarWatchController> logger)
+    {
+        _logger = logger;
+    }
+
+
+
+    [HttpGet(Name = "SolarWatch")]
+    public ActionResult<SolarWatch> Get()
+    {
+        try
+        {
+            return new SolarWatch 
+            { 
+                Date = DateTime.Now, 
+                City = "Miskolc",
+                Sunset = new TimeOnly(6, 30), 
+                Sunrise = new TimeOnly(20, 30) 
+            };
+            
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "Error getting data");
+            return NotFound("Error getting data");
+        }
+    }
+}
