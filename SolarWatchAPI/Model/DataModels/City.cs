2aa20000c19c0c741fa7e437892b8fa92726df9f
ndrs06 <szaki1006@gmail.com>
2024-05-31 15:30:25 +0200

    :seedling: chore: added navigation property to City data model

--- SolarWatchAPI/Model/DataModels/City.cs
+++ SolarWatchAPI/Model/DataModels/City.cs
@@ -11,4 +11,6 @@ public class City
     public double Lon { get; init; }
     public string State { get; init; }
     public string Coutry { get; init; }
+    
+    public ICollection<SunriseSunset> SunriseSunsets { get; init; }
 }
