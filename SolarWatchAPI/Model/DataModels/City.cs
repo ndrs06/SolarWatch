7349b70ea77226868e13f37f1e938934161ef7af
ndrs06 <szaki1006@gmail.com>
2024-05-31 14:52:59 +0200

    :seedling: chore: created City data model

---
+++ SolarWatchAPI/Model/DataModels/City.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace SolarWatchAPI.Model.DataModels;
+
+public class City
+{
+    [Key]
+    public int Id { get; init; }
+    public string Name { get; init; }
+    public double Lat { get; init; }
+    public double Lon { get; init; }
+    public string State { get; init; }
+    public string Coutry { get; init; }
+}
