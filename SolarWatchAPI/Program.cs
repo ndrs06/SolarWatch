0d392730ef1eaca0a2f7078c82e6986c85dc32dc
ndrs06 <szaki1006@gmail.com>
2024-06-02 17:39:54 +0200

    :heavy_plus_sign: chore: added services classes into Program.cs

--- SolarWatchAPI/Program.cs
+++ SolarWatchAPI/Program.cs
@@ -1,4 +1,5 @@
 using SolarWatchAPI.Data;
+using SolarWatchAPI.Service;
 using SolarWatchAPI.Service.DataProviders;
 using SolarWatchAPI.Service.JsonProcessors;
 using SolarWatchAPI.Service.Repository;
@@ -15,11 +16,17 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<SolarWatchApiContext>();
 
 builder.Services.AddSingleton<ICoordinatesProvider, CoordinatesProvider>();
-builder.Services.AddSingleton<ISolarWatchDataProvider, SolarWatchDataProvider>();
+builder.Services.AddSingleton<ISolarWatchProvider, SolarWatchProvider>();
+
 builder.Services.AddSingleton<IJsonProcessor, JsonProcessor>();
 
 builder.Services.AddScoped<ICityRepository, CityRepository>();
 builder.Services.AddScoped<ISunriseSunsetRepository, SunsetSunriseRepository>();
+
+builder.Services.AddSingleton<ICityService, CityService>();
+builder.Services.AddSingleton<ISunriseSunsetService, SunriseSunsetService>();
+builder.Services.AddSingleton<ICoordinatesService, CoordinatesService>();
+builder.Services.AddSingleton<ISolarWatchService, SolarWatchService>();
 
 var app = builder.Build();
 
