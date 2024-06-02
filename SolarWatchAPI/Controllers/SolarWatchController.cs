7c701b991ba4c3c4c58d7d6c1e248bc436b20375
ndrs06 <szaki1006@gmail.com>
2024-06-02 17:35:56 +0200

    :recycle: refactor: implemented services classes into SolarWatchController

--- SolarWatchAPI/Controllers/SolarWatchController.cs
+++ SolarWatchAPI/Controllers/SolarWatchController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using SolarWatchAPI.Model;
 using SolarWatchAPI.Model.DataModels;
+using SolarWatchAPI.Service;
 using SolarWatchAPI.Service.DataProviders;
 using SolarWatchAPI.Service.JsonProcessors;
 using SolarWatchAPI.Service.Repository;
@@ -13,65 +14,52 @@ namespace SolarWatchAPI.Controllers;
 public class SolarWatchController : ControllerBase
 {
     private readonly ILogger<SolarWatchController> _logger;
-    private readonly ICoordinatesProvider _coordinatesProvider;
-    private readonly ISolarWatchDataProvider _solarWatchDataProvider;
-    private readonly IJsonProcessor _jsonProcessor;
-    private readonly ICityRepository _cityRepository;
-    private readonly ISunriseSunsetRepository _sunriseSunsetRepository;
-
-    public SolarWatchController(ILogger<SolarWatchController> logger, ICoordinatesProvider coordinatesProvider, ISolarWatchDataProvider solarWatchDataProvider, IJsonProcessor jsonProcessor, ICityRepository cityRepository, ISunriseSunsetRepository sunriseSunsetRepository)
-    {
-        _logger = logger;
-        _coordinatesProvider = coordinatesProvider;
-        _solarWatchDataProvider = solarWatchDataProvider;
-        _jsonProcessor = jsonProcessor;
-        _cityRepository = cityRepository;
-        _sunriseSunsetRepository = sunriseSunsetRepository;
-    }
+    private readonly ICityService _cityService;
+    private readonly ISunriseSunsetService _sunriseSunsetService;
+    private readonly ICoordinatesService _coordinatesService;
+    private readonly ISolarWatchService _solarWatchService;
     
     // DbContext
     [HttpGet(Name = "SolarWatch")]
-    public async Task<ActionResult<SolarWatch>> Get(string city, DateTime date)
+    public async Task<ActionResult<SolarWatch>> Get(string cityName, DateTime date)
     {
         Coordinates coordinates;
-        var dbCity = _cityRepository.GetByName(city);
+        var dbCity = _cityService.GetByName(cityName);
 
         if (dbCity == null)
         {
-            _logger.LogInformation($"DB does not contains city with this name: {city}");
+            _logger.LogInformation($"DB does not contains city with this name: {cityName}");
             try
             {
-                var coordinatesData = await _coordinatesProvider.GetCoordinates(city);
-                coordinates = _jsonProcessor.ProcessCoordinates(coordinatesData);
+                coordinates = await _coordinatesService.GetAsync(cityName);
                 
                 var newCity = new City
                 {
-                    Name = city,
+                    Name = cityName,
                     Lat = coordinates.Lat,
                     Lon = coordinates.Lon
                 };
-                _cityRepository.Add(newCity);
+                _cityService.Add(newCity);
             }
             catch (Exception e)
             {
                 _logger.LogError(e.Message);
-                return NotFound($"Not found city with this name: {city}");
+                return NotFound($"Not found city with this name: {cityName}");
             }
             
             try
             {
-                var solarWatchData = await _solarWatchDataProvider.GetCurrent(date, coordinates.Lat, coordinates.Lon);
-                var solarWatch = _jsonProcessor.ProcessSolarWatch(solarWatchData);
-                solarWatch.City = city;
+                var solarWatch = await _solarWatchService.GetAsync(date, coordinates);
+                solarWatch.City = cityName;
 
                 var newSunriseSunset = new SunriseSunset
                 {
                     Date = solarWatch.Date,
                     Sunrise = solarWatch.Sunrise,
                     Sunset = solarWatch.Sunset,
-                    CityId = _cityRepository.GetByName(solarWatch.City).Id
+                    CityId = _cityService.GetByName(solarWatch.City).Id
                 };
-                _sunriseSunsetRepository.Add(newSunriseSunset);
+                _sunriseSunsetService.Add(newSunriseSunset);
                 
                 return Ok(solarWatch);
             }
@@ -83,25 +71,24 @@ public class SolarWatchController : ControllerBase
         }
         
         coordinates = new Coordinates{Lat = dbCity.Lat, Lon = dbCity.Lon};
-        var dbSunriseSunset = _sunriseSunsetRepository.GetByCityAndDate(dbCity.Id, date);
+        var dbSunriseSunset = _sunriseSunsetService.GetByCityIdAndDate(dbCity.Id, date);
             
         if (dbSunriseSunset == null)
         {
             try
             {
-                var solarWatchData = await _solarWatchDataProvider.GetCurrent(date, coordinates.Lat, coordinates.Lon);
-                var solarWatch = _jsonProcessor.ProcessSolarWatch(solarWatchData);
-                solarWatch.City = city;
+                var solarWatch = await _solarWatchService.GetAsync(date, coordinates);
+                solarWatch.City = cityName;
 
                 var newSunriseSunset = new SunriseSunset
                 {
                     Date = solarWatch.Date,
                     Sunrise = solarWatch.Sunrise,
                     Sunset = solarWatch.Sunset,
-                    CityId = _cityRepository.GetByName(solarWatch.City).Id,
-                    City = _cityRepository.GetByName(solarWatch.City)
+                    CityId = _cityService.GetByName(solarWatch.City).Id,
+                    City = _cityService.GetByName(solarWatch.City)
                 };
-                _sunriseSunsetRepository.Add(newSunriseSunset);
+                _sunriseSunsetService.Add(newSunriseSunset);
                 
                 return Ok(solarWatch);
             }
