d37559e0091ee4fc70556d716e22ac65627d1a20
ndrs06 <szaki1006@gmail.com>
2024-06-03 16:36:47 +0200

    :bug: fix: added constructor to SolarWatchController.cs & refactored Get method

--- SolarWatchAPI/Controllers/SolarWatchController.cs
+++ SolarWatchAPI/Controllers/SolarWatchController.cs
@@ -18,9 +18,97 @@ public class SolarWatchController : ControllerBase
     private readonly ISunriseSunsetService _sunriseSunsetService;
     private readonly ICoordinatesService _coordinatesService;
     private readonly ISolarWatchService _solarWatchService;
+
+    public SolarWatchController(ILogger<SolarWatchController> logger, ICityService cityService, ISunriseSunsetService sunriseSunsetService, ICoordinatesService coordinatesService, ISolarWatchService solarWatchService)
+    {
+        _logger = logger;
+        _cityService = cityService;
+        _sunriseSunsetService = sunriseSunsetService;
+        _coordinatesService = coordinatesService;
+        _solarWatchService = solarWatchService;
+    }
+
+    [HttpGet(Name = "SolarWatch")]
+    public async Task<ActionResult<SolarWatch>> Get(string cityName, DateTime date)
+    {
+        try
+        {
+            Coordinates coordinates;
+            var dbCity = _cityService.GetByName(cityName);
+
+            if (dbCity == null)
+            {
+                _logger.LogInformation($"DB does not contains city with this name: {cityName}");
+                try
+                {
+                    coordinates = await _coordinatesService.GetAsync(cityName);
+
+                    var newCity = new City
+                    {
+                        Name = cityName,
+                        Lat = coordinates.Lat,
+                        Lon = coordinates.Lon
+                    };
+                    _cityService.Add(newCity);
+                }
+                catch (Exception e)
+                {
+                    _logger.LogError(e, ":)");
+                    return NotFound($"Not found city with this name: {cityName}");
+                }
+            }
+            else
+            {
+                coordinates = new Coordinates { Lat = dbCity.Lat, Lon = dbCity.Lon };
+            }
+
+            var dbSunriseSunset = _sunriseSunsetService.GetByCityIdAndDate(dbCity.Id, date);
+
+            if (dbSunriseSunset == null)
+            {
+                try
+                {
+                    var solarWatch = await _solarWatchService.GetAsync(date, coordinates);
+                    solarWatch.City = cityName;
+
+                    var newSunriseSunset = new SunriseSunset
+                    {
+                        Date = solarWatch.Date,
+                        Sunrise = solarWatch.Sunrise,
+                        Sunset = solarWatch.Sunset,
+                        CityId = _cityService.GetByName(solarWatch.City).Id,
+                        City = _cityService.GetByName(solarWatch.City)
+                    };
+                    _sunriseSunsetService.Add(newSunriseSunset);
+
+                    return Ok(solarWatch);
+                }
+                catch (Exception e)
+                {
+                    _logger.LogError(e.Message);
+                    return NotFound("Error getting data");
+                }
+            }
+            
+            return Ok(new SolarWatch
+            {
+                City = dbCity.Name,
+                Date = dbSunriseSunset.Date,
+                Sunrise = dbSunriseSunset.Sunrise,
+                Sunset = dbSunriseSunset.Sunset
+            });
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+
     
     // DbContext
-    [HttpGet(Name = "SolarWatch")]
+    /*[HttpGet(Name = "SolarWatch")]
     public async Task<ActionResult<SolarWatch>> Get(string cityName, DateTime date)
     {
         Coordinates coordinates;
@@ -107,7 +195,7 @@ public class SolarWatchController : ControllerBase
             Sunset = dbSunriseSunset.Sunset
         });
         
-    }
+    }*/
     
     /*[HttpGet(Name = "SolarWatch")]
     public async Task<ActionResult<SolarWatch>> Get(string city, DateTime date)
@@ -138,3 +226,4 @@ public class SolarWatchController : ControllerBase
         }
     }*/
 }
+
