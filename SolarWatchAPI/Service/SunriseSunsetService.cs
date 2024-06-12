5609156f4608d7d8ee03a76246439c3f7391fe7b
ndrs06 <szaki1006@gmail.com>
2024-06-12 15:51:46 +0200

    :building_construction: refactor: implemented architectural changes in SunriseSunsetService.cs

--- SolarWatchAPI/Service/SunriseSunsetService.cs
+++ SolarWatchAPI/Service/SunriseSunsetService.cs
@@ -1,4 +1,7 @@
+using SolarWatchAPI.Model;
 using SolarWatchAPI.Model.DataModels;
+using SolarWatchAPI.Service.DataProviders;
+using SolarWatchAPI.Service.JsonProcessors;
 using SolarWatchAPI.Service.Repository;
 
 namespace SolarWatchAPI.Service;
@@ -7,20 +10,35 @@ public class SunriseSunsetService : ISunriseSunsetService
 {
     private readonly ILogger<SunriseSunsetService> _logger;
     private readonly ISunriseSunsetRepository _sunriseSunsetRepository;
+    private readonly ISunriseSunsetApiDataProvider _sunriseSunsetApiDataProvider;
+    private readonly IJsonProcessor _jsonProcessor;
+    private string? SunriseSunsetJsonData  { get; set; }
 
-    public SunriseSunsetService(ILogger<SunriseSunsetService> logger, ISunriseSunsetRepository sunriseSunsetRepository)
+    public SunriseSunsetService(ILogger<SunriseSunsetService> logger, ISunriseSunsetRepository sunriseSunsetRepository, ISunriseSunsetApiDataProvider sunriseSunsetApiDataProvider, IJsonProcessor jsonProcessor)
     {
         _logger = logger;
         _sunriseSunsetRepository = sunriseSunsetRepository;
+        _sunriseSunsetApiDataProvider = sunriseSunsetApiDataProvider;
+        _jsonProcessor = jsonProcessor;
     }
 
-    public SunriseSunset? GetByCityIdAndDate(int cityId, DateTime date)
+    public SunriseSunset? GetByCityIdAndDate(string cityName, DateTime date)
     {
-        return _sunriseSunsetRepository.GetByCityAndDate(cityId, date);
+        return _sunriseSunsetRepository.GetByCityAndDate(cityName, date);
     }
-
-    public void Add(SunriseSunset sunriseSunset)
+    
+    public void AddSunriseSunsetToDb(string cityName)
     {
+        var sunriseSunset = _jsonProcessor.ProcessSunriseSunset(SunriseSunsetJsonData);
+        sunriseSunset.CityName = cityName;
+        
         _sunriseSunsetRepository.Add(sunriseSunset);
+    }
+
+    public async Task<SolarWatch> GetSolarWatchAsync(DateTime date, Coordinates coordinates)
+    {
+        SunriseSunsetJsonData = await _sunriseSunsetApiDataProvider.GetAsync(date, coordinates);
+
+        return _jsonProcessor.ProcessSolarWatch(SunriseSunsetJsonData);
     }
 }
