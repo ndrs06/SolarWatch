353009a03e73d8c3256acf83699503276501f380
ndrs06 <szaki1006@gmail.com>
2024-06-12 15:51:31 +0200

    :building_construction: refactor: implemented architectural changes in CityService.cs

--- SolarWatchAPI/Service/CityService.cs
+++ SolarWatchAPI/Service/CityService.cs
@@ -1,4 +1,7 @@
+using SolarWatchAPI.Model;
 using SolarWatchAPI.Model.DataModels;
+using SolarWatchAPI.Service.DataProviders;
+using SolarWatchAPI.Service.JsonProcessors;
 using SolarWatchAPI.Service.Repository;
 
 namespace SolarWatchAPI.Service;
@@ -7,20 +10,34 @@ public class CityService : ICityService
 {
     private readonly ILogger<CityService> _logger;
     private readonly ICityRepository _cityRepository;
+    private readonly IOpenWeatherMapApiDataProvider _openWeatherMapApiDataProvider;
+    private readonly IJsonProcessor _jsonProcessor;
+    private string? OpenWeatherJsonData { get; set; }
 
-    public CityService(ILogger<CityService> logger, ICityRepository cityRepository)
+    public CityService(ILogger<CityService> logger, ICityRepository cityRepository, IJsonProcessor jsonProcessor, IOpenWeatherMapApiDataProvider openWeatherMapApiDataProvider)
     {
         _logger = logger;
         _cityRepository = cityRepository;
+        _jsonProcessor = jsonProcessor;
+        _openWeatherMapApiDataProvider = openWeatherMapApiDataProvider;
     }
 
-    public City? GetByName(string cityName)
+    public City? GetByName(string? cityName)
     {
         return _cityRepository.GetByName(cityName);
     }
 
-    public void Add(City city)
+    public void AddCityToDb()
     {
+        var city = _jsonProcessor.ProcessCity(OpenWeatherJsonData);
+        
         _cityRepository.Add(city);
+    }
+
+    public async Task<Coordinates> GetCityCoordinatesAsync(string? cityName)
+    {
+        OpenWeatherJsonData = await _openWeatherMapApiDataProvider.GetAsync(cityName);
+        
+        return _jsonProcessor.ProcessCoordinates(OpenWeatherJsonData);
     }
 }
