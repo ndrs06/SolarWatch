0466fac14ea46e7154cf6f075169bd7e65780263
ndrs06 <szaki1006@gmail.com>
2024-06-12 15:38:01 +0200

    :card_file_box: refactor: regenerated migrations

---
+++ SolarWatchAPI/Migrations/20240612130405_InitialDB.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SolarWatchAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialDB : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cities",
+                columns: table => new
+                {
+                    Name = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Lat = table.Column<double>(type: "float", nullable: false),
+                    Lon = table.Column<double>(type: "float", nullable: false),
+                    State = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Coutry = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cities", x => x.Name);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SunriseSunsets",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Sunrise = table.Column<TimeOnly>(type: "time", nullable: false),
+                    Sunset = table.Column<TimeOnly>(type: "time", nullable: false),
+                    CityName = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SunriseSunsets", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SunriseSunsets_Cities_CityName",
+                        column: x => x.CityName,
+                        principalTable: "Cities",
+                        principalColumn: "Name",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SunriseSunsets_CityName",
+                table: "SunriseSunsets",
+                column: "CityName");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SunriseSunsets");
+
+            migrationBuilder.DropTable(
+                name: "Cities");
+        }
+    }
+}
