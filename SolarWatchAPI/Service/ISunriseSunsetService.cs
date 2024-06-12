361245d816fcf2c1c885d563f5258204e70e017f
ndrs06 <szaki1006@gmail.com>
2024-06-12 15:42:43 +0200

    :building_construction: chore: added GetSolarWatchAsync method to ISunriseSunsetService.cs

--- SolarWatchAPI/Service/ISunriseSunsetService.cs
+++ SolarWatchAPI/Service/ISunriseSunsetService.cs
@@ -1,9 +1,11 @@
+using SolarWatchAPI.Model;
 using SolarWatchAPI.Model.DataModels;
 
 namespace SolarWatchAPI.Service;
 
 public interface ISunriseSunsetService
 {
-    SunriseSunset? GetByCityIdAndDate(int cityId, DateTime date);
-    void Add(SunriseSunset sunriseSunset);
+    SunriseSunset? GetByCityIdAndDate(string cityName, DateTime date);
+    void AddSunriseSunsetToDb(string cityName);
+    Task<SolarWatch> GetSolarWatchAsync(DateTime date, Coordinates coordinates);
 }
