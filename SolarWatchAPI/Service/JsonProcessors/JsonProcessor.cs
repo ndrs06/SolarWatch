a337ce736ed0372cb1d86683c7baa09fbbaafa50
ndrs06 <szaki1006@gmail.com>
2024-06-12 15:44:38 +0200

    :building_construction: chore: implemented ProcessCity and ProcessSunriseSunset in JsonProcessor.cs

--- SolarWatchAPI/Service/JsonProcessors/JsonProcessor.cs
+++ SolarWatchAPI/Service/JsonProcessors/JsonProcessor.cs
@@ -1,5 +1,6 @@
 using System.Text.Json;
 using SolarWatchAPI.Model;
+using SolarWatchAPI.Model.DataModels;
 
 namespace SolarWatchAPI.Service.JsonProcessors;
 
@@ -41,14 +42,48 @@ public class JsonProcessor : IJsonProcessor
         return solarWatch;
     }
 
-    private DateTime GetDateTimeFromString(string dateStr)
+    public City ProcessCity(string data)
+    {
+        JsonDocument json = JsonDocument.Parse(data);
+        _logger.LogInformation("City JSON data parsed");
+        
+        var cityElement = json.RootElement[0];
+        
+        var city = new City
+        {
+            Name = cityElement.TryGetProperty("name", out JsonElement nameElement) ? nameElement.GetString() : "Unknown",
+            Lat = cityElement.TryGetProperty("lat", out JsonElement latElement) ? latElement.GetDouble() : 0.0,
+            Lon = cityElement.TryGetProperty("lon", out JsonElement lonElement) ? lonElement.GetDouble() : 0.0,
+            State = cityElement.TryGetProperty("state", out JsonElement stateElement) ? stateElement.GetString() ?? "NAN" : "NAN",
+            Coutry = cityElement.TryGetProperty("country", out JsonElement countryElement) ? countryElement.GetString() ?? "NAN" : "NAN",
+        };
+
+        return city;
+    }
+
+    public SunriseSunset ProcessSunriseSunset(string data)
+    {
+        JsonDocument json = JsonDocument.Parse(data);
+        JsonElement result = json.RootElement.GetProperty("results");
+
+        var sunriseSunset = new SunriseSunset
+        {
+            Date = GetDateTimeFromString(result.GetProperty("sunrise").GetString()),
+            Sunrise = GetTimeOnlyFromString(result.GetProperty("sunrise").GetString()),
+            Sunset = GetTimeOnlyFromString(result.GetProperty("sunset").GetString())
+        };
+
+        return sunriseSunset;
+    }
+
+    private DateTime GetDateTimeFromString(string? dateStr)
     {
         var dateTime = DateTime.Parse(dateStr[..10]);
 
         return dateTime;
     }
 
-    private TimeOnly GetTimeOnlyFromString(string dateStr)
+    private TimeOnly GetTimeOnlyFromString(string? dateStr)
     {
         var time = DateTimeOffset.Parse(dateStr);
         var timeOnly = new TimeOnly(time.TimeOfDay.Hours, time.TimeOfDay.Minutes);
