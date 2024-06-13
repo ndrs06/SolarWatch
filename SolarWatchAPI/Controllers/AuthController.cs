31060afa42b7f52d8e58da7b896bdaccdaf2033b
ndrs06 <szaki1006@gmail.com>
2024-06-13 10:57:16 +0200

    :passport_control: chore: implemented AuthController with reg, login endpoints

---
+++ SolarWatchAPI/Controllers/AuthController.cs
@@ -0,0 +1,67 @@
+using Microsoft.AspNetCore.Mvc;
+using SolarWatchAPI.Contracts;
+using SolarWatchAPI.Model.Authentication;
+using SolarWatchAPI.Service.Authentication;
+
+namespace SolarWatchAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class AuthController : ControllerBase
+{
+    private readonly IAuthService _authenticationService;
+    private readonly IConfiguration _configuration;
+
+    public AuthController(IAuthService authenticationService, IConfiguration configuration)
+    {
+        _authenticationService = authenticationService;
+        _configuration = configuration;
+    }
+    
+    [HttpPost("Registration")]
+    public async Task<ActionResult<RegistrationResponse>> Register([FromBody] RegistrationRequest request)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+
+        var jwtSettings = _configuration.GetSection("JwtSettings");
+        var result = await _authenticationService.RegisterAsync(request.Email, request.UserName, request.Password, jwtSettings.GetSection("Roles").Get<string[]>()[0]);
+
+        if (!result.Success)
+        {
+            AddErrors(result);
+            return BadRequest(ModelState);
+        }
+
+        return CreatedAtAction(nameof(Register), new RegistrationResponse(result.Email, result.UserName));
+    }
+    
+    [HttpPost("Login")]
+    public async Task<ActionResult<AuthResponse>> Authenticate([FromBody] AuthRequest request)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+
+        var result = await _authenticationService.LoginAsync(request.Email, request.Password);
+
+        if (!result.Success)
+        {
+            AddErrors(result);
+            return BadRequest(ModelState);
+        }
+
+        return Ok(new AuthResponse(result.Email, result.UserName, result.Token));
+    }
+    
+    private void AddErrors(AuthResult result)
+    {
+        foreach (var err in result.ErrorMessages)
+        {
+            ModelState.AddModelError(err.Key, err.Value);
+        }
+    }
+}
