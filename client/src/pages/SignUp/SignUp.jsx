45ac47f7b15084269b3eb845917ffbb3c84ded03
ndrs <szaki1006@gmail.com>
2024-06-14 12:51:30 +0200

    :bento: chore: implemented SignUp

--- client/src/pages/SignUp/SignUp.jsx
+++ client/src/pages/SignUp/SignUp.jsx
@@ -1,12 +1,42 @@
 import './SignUp.scss';
 import React from 'react';
+import {useNavigate} from "react-router-dom";
 
 import Navbar from "../../components/Navbar/Navbar.jsx";
+import SignUpForm from "../../components/SignUpForm/SignUpForm.jsx"
+
+const postSignUp = (user) => {
+    return fetch('/api/Auth/Registration', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(user)
+    }).then(res => {
+        if (!res.ok) {
+            throw new Error(`HTTP error! status: ${res.status}`);
+        }
+        return res.ok
+    }).catch(err => {
+        console.error('Error:', err);
+    });
+}
 
 export default function SignUp() {
+    const navigate = useNavigate();
+
+    const handleSignIn = (user) => {
+        postSignUp(user).then( res => res ? navigate("/solar-watch") : navigate("/sign-up"))
+    }
+
+    const props = {
+        onSave: handleSignIn,
+        onCancel: _ => navigate("/")
+    }
     return (
         <>
             <Navbar/>
+            <SignUpForm {...props}/>
         </>
     );
 }
