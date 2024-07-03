222b8254fb568cab92201496e752062f9cf27c60
ndrs <szaki1006@gmail.com>
2024-07-03 12:16:29 +0200

    :bento: chore: created bone of SolarWatchResult.jsx component

---
+++ client/src/components/SolarWatchResult/SolarWatchResult.jsx
@@ -0,0 +1,14 @@
+import './SolarWatchResult.scss';
+import React from "react";
+
+export default function SolarWatchResult(props) {
+    const {
+
+    } = props
+   
+    return (
+        <>
+        </>
+    )
+
+}
