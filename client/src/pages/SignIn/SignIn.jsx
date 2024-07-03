77e85dc4cc43e74e0b0837f280dcd3e066e47c85
ndrs <szaki1006@gmail.com>
2024-07-03 12:14:03 +0200

    :necktie: chore: extended handleSignIn with login method of profileContext

--- client/src/pages/SignIn/SignIn.jsx
+++ client/src/pages/SignIn/SignIn.jsx
@@ -1,6 +1,7 @@
 import './SignIn.scss';
 import React from 'react';
 import {useNavigate} from "react-router-dom";
+import { useProfile } from '../../contexts/ProfileContext';
 
 import Navbar from "../../components/Navbar/Navbar.jsx";
 import SignInForm from "../../components/SignInForm/SignInForm.jsx";
@@ -25,9 +26,17 @@ const postSignIn = (user) => {
 
 export default function SignIn() {
     const navigate = useNavigate();
+    const { login } = useProfile();
     
     const handleSignIn = (user) => {
-        postSignIn(user).then( res => res ? navigate("/solar-watch") : navigate("/sign-in"))
+        postSignIn(user).then( res => {
+            if (!res)
+            {
+                navigate("/sign-in")                
+            }
+            login();
+            navigate("/solar-watch")            
+        })
     }
     
     const props = {
