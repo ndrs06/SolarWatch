e14431e4ffa6d8726e77b0b8a24c06084eacf078
ndrs <szaki1006@gmail.com>
2024-07-05 10:08:29 +0200

    :recycle: refactor: IdentityDbContext passed into SolarWatchApiContext.cs

--- SolarWatchAPI/Data/SolarWatchApiContext.cs
+++ SolarWatchAPI/Data/SolarWatchApiContext.cs
@@ -1,15 +1,17 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using SolarWatchAPI.Model.DataModels;
 
 namespace SolarWatchAPI.Data;
 
-public class SolarWatchApiContext : DbContext
+public class SolarWatchApiContext : IdentityDbContext<IdentityUser, IdentityRole, string>
 {
     public DbSet<City> Cities { get; set; }
     public DbSet<SunriseSunset> SunriseSunsets { get; set; }
     private readonly IConfiguration _configuration;
 
-    public SolarWatchApiContext(IConfiguration configuration)
+    public SolarWatchApiContext(DbContextOptions<SolarWatchApiContext> options, IConfiguration configuration) : base(options)
     {
         _configuration = configuration;
     }
