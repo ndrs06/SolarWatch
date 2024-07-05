c07dc4f96efa74704efa0cf834dd19a2dede9c28
ndrs <szaki1006@gmail.com>
2024-07-05 15:25:22 +0200

    :white_check_mark: test: added full coverage test to SolarWatchController

--- SolarWatchAPITest/SolarWatchAPITest/SolarWatchControllerTest.cs
+++ SolarWatchAPITest/SolarWatchAPITest/SolarWatchControllerTest.cs
@@ -4,9 +4,8 @@ using Moq;
 using NUnit.Framework;
 using SolarWatchAPI.Controllers;
 using SolarWatchAPI.Model;
+using SolarWatchAPI.Model.DataModels;
 using SolarWatchAPI.Service;
-using SolarWatchAPI.Service.DataProviders;
-using SolarWatchAPI.Service.JsonProcessors;
 
 namespace SolarWatchAPITest;
 
@@ -28,89 +27,220 @@ public class SolarWatchControllerTest
     }
 
     [Test]
-    public async Task Get_ReturnsNotFoundIfCityNameIsInvalid()
+    public async Task GetSolarWatch_ShouldReturnOk_WhenDbSunriseSunsetExists()
     {
         // Arrange
-        _cityServiceMock.Setup(x => x.GetCityCoordinatesAsync(It.IsAny<string>())).Throws(new Exception());
-        var city = "kdjhfélsjhédg";
-        var date = DateTime.Today;
+        var cityName = "TestCity";
+        var date = DateTime.Now;
+        var dbSunriseSunset = new SunriseSunset();
+        _sunriseSunsetServiceMock.Setup(service => service.GetByCityNameAndDate(It.IsAny<string>(), It.IsAny<DateTime>()))
+            .Returns(dbSunriseSunset);
         
         // Act
-        var res = await _solarWatchController.Get(city, date);
+        var res = await _solarWatchController.GetSolarWatch(cityName, date);
         
         // Assert
-        Assert.IsInstanceOf(typeof(NotFoundObjectResult), res.Result);
-    }
-    
-    [Test]
-    public async Task Get_ReturnsNotFoundIfCoordinateDataIsInvalid()
-    {
-        // Arrange
-        var coordinates = new Coordinates();
-        _cityServiceMock.Setup(x => x.GetCityCoordinatesAsync(It.IsAny<string>())).ReturnsAsync(coordinates);
-        
-        var city = "Miskolc";
-        var date = DateTime.Today;
-        
-        // Act
-        var res = await _solarWatchController.Get(city, date);
-        
-        // Assert
-        Assert.IsInstanceOf(typeof(NotFoundObjectResult), res.Result);
-    }
-    
-    [Test]
-    public async Task Get_ReturnsNotFoundIfCoordinatesIsInvalid()
-    {
-        // Arrange
-        _sunriseSunsetServiceMock.Setup(x => x.GetSolarWatchAsync(It.IsAny<DateTime>(), It.IsAny<Coordinates>())).Throws(new Exception());
-        var city = "Miskolc";
-        var date = DateTime.Today;
-        
-        // Act
-        var res = await _solarWatchController.Get(city, date);
-        
-        // Assert
-        Assert.IsInstanceOf(typeof(NotFoundObjectResult), res.Result);
-    }
-    
-    [Test]
-    public async Task Get_ReturnsNotFoundIfSolarWatchDataIsInvalid()
-    {
-        // Arrange
-        var solarWatch = new SolarWatch();
-        _sunriseSunsetServiceMock.Setup(x => x.GetSolarWatchAsync(It.IsAny<DateTime>(), It.IsAny<Coordinates>())).ReturnsAsync(solarWatch);
-        
-        var city = "Miskolc";
-        var date = DateTime.Today;
-        
-        // Act
-        var res = await _solarWatchController.Get(city, date);
-        
-        // Assert
-        Assert.IsInstanceOf(typeof(NotFoundObjectResult), res.Result);
-    }
-
-    [Test]
-    public async Task Get_ReturnsOkIfAllDataAreValid()
-    {
-        // Arrange
-
-        var expectedCoordinates = new Coordinates();
-        var expectedSolarWatch = new SolarWatch();
-        var data = "{}";
-        _cityServiceMock.Setup(x => x.GetCityCoordinatesAsync(It.IsAny<string>())).ReturnsAsync(expectedCoordinates);
-        _sunriseSunsetServiceMock
-            .Setup(x => x.GetSolarWatchAsync(It.IsAny<DateTime>(), It.IsAny<Coordinates>())).ReturnsAsync(expectedSolarWatch);
-        
-        var city = "Miskolc";
-        var date = DateTime.Today;
-        
-        // Act
-        var res = await _solarWatchController.Get(city, date);
-        
-        // Arrange
         Assert.IsInstanceOf(typeof(OkObjectResult), res.Result);
+    }
+    
+    [Test]
+    public async Task GetSolarWatch_ShouldReturnBadRequest_WhenExceptionInGetOpenWeatherMapApiDataAsync()
+    {
+        // Arrange
+        var cityName = "TestCity";
+        var date = DateTime.UtcNow;
+        _sunriseSunsetServiceMock.Setup(service => service.GetByCityNameAndDate(cityName, date)).Returns((SunriseSunset)null!);
+        _cityServiceMock.Setup(service => service.GetByName(cityName)).Returns((City)null!);
+        _cityServiceMock.Setup(service => service.GetOpenWeatherMapApiDataAsync(cityName)).ThrowsAsync(new Exception());
 
+        // Act
+        var res = await _solarWatchController.GetSolarWatch(cityName, date);
+
+        // Assert
+        Assert.IsInstanceOf(typeof(BadRequestObjectResult), res.Result);
+    }
+
+    [Test]
+    public async Task GetSolarWatch_ShouldReturnNotFound_WhenExceptionInGetSunriseSunsetApiDataAsync()
+    {
+        // Arrange
+        var cityName = "TestCity";
+        var date = DateTime.UtcNow;
+        var coordinates = new Coordinates();
+        _sunriseSunsetServiceMock.Setup(service => service.GetByCityNameAndDate(cityName, date)).Returns((SunriseSunset)null!);
+        _cityServiceMock.Setup(service => service.GetByName(cityName)).Returns((City)null!);
+        _cityServiceMock.Setup(service => service.GetOpenWeatherMapApiDataAsync(cityName)).ReturnsAsync("jsonData");
+        _cityServiceMock.Setup(service => service.ProcessCityCoordinates("jsonData")).Returns(coordinates);
+        _sunriseSunsetServiceMock.Setup(service => service.GetSunriseSunsetApiDataAsync(date, coordinates)).ThrowsAsync(new Exception());
+
+        // Act
+        var res = await _solarWatchController.GetSolarWatch(cityName, date);
+
+        // Assert
+        Assert.IsInstanceOf(typeof(NotFoundObjectResult), res.Result);
+    }
+
+    [Test]
+    public async Task GetSolarWatch_ShouldReturnBadRequest_WhenExceptionInAddCityToDb()
+    {
+        // Arrange
+        var cityName = "TestCity";
+        var date = DateTime.UtcNow;
+        var coordinates = new Coordinates();
+        _sunriseSunsetServiceMock.Setup(service => service.GetByCityNameAndDate(cityName, date)).Returns((SunriseSunset)null!);
+        _cityServiceMock.Setup(service => service.GetByName(cityName)).Returns((City)null!);
+        _cityServiceMock.Setup(service => service.GetOpenWeatherMapApiDataAsync(cityName)).ReturnsAsync("jsonData");
+        _cityServiceMock.Setup(service => service.ProcessCityCoordinates("jsonData")).Returns(coordinates);
+        _cityServiceMock.Setup(service => service.ProcessCity("jsonData")).Throws(new Exception());
+
+        // Act
+        var res = await _solarWatchController.GetSolarWatch(cityName, date);
+
+        // Assert
+        Assert.IsInstanceOf(typeof(BadRequestObjectResult), res.Result);
+    }
+
+    [Test]
+    public async Task GetSolarWatch_ShouldReturnBadRequest_WhenExceptionInAddSunriseSunsetToDb()
+    {
+        // Arrange
+        var cityName = "TestCity";
+        var date = DateTime.UtcNow;
+        var coordinates = new Coordinates();
+        _sunriseSunsetServiceMock.Setup(service => service.GetByCityNameAndDate(cityName, date)).Returns((SunriseSunset)null!);
+        _cityServiceMock.Setup(service => service.GetByName(cityName)).Returns((City)null!);
+        _cityServiceMock.Setup(service => service.GetOpenWeatherMapApiDataAsync(cityName)).ReturnsAsync("jsonData");
+        _cityServiceMock.Setup(service => service.ProcessCityCoordinates("jsonData")).Returns(coordinates);
+        _cityServiceMock.Setup(service => service.ProcessCity("jsonData")).Returns(new City());
+        _sunriseSunsetServiceMock.Setup(service => service.GetSunriseSunsetApiDataAsync(date, coordinates)).ReturnsAsync("jsonData");
+        _sunriseSunsetServiceMock.Setup(service => service.ProcessSunriseSunset("jsonData")).Returns(new SunriseSunset());
+        _sunriseSunsetServiceMock.Setup(service => service.AddSunriseSunsetToDb(It.IsAny<SunriseSunset>())).Throws(new Exception());
+
+        // Act
+        var res = await _solarWatchController.GetSolarWatch(cityName, date);
+
+        // Assert
+        Assert.IsInstanceOf(typeof(BadRequestObjectResult), res.Result);
+    }
+
+    [Test]
+    public async Task GetSolarWatch_ShouldReturnOk_WhenSunriseSunsetDataProcessed()
+    {
+        // Arrange
+        var cityName = "TestCity";
+        var date = DateTime.UtcNow;
+        var coordinates = new Coordinates();
+        _sunriseSunsetServiceMock.Setup(service => service.GetByCityNameAndDate(cityName, date)).Returns((SunriseSunset)null!);
+        _cityServiceMock.Setup(service => service.GetByName(cityName)).Returns((City)null!);
+        _cityServiceMock.Setup(service => service.GetOpenWeatherMapApiDataAsync(cityName)).ReturnsAsync("jsonData");
+        _cityServiceMock.Setup(service => service.ProcessCityCoordinates("jsonData")).Returns(coordinates);
+        _cityServiceMock.Setup(service => service.ProcessCity("jsonData")).Returns(new City());
+        _sunriseSunsetServiceMock.Setup(service => service.GetSunriseSunsetApiDataAsync(date, coordinates)).ReturnsAsync("jsonData");
+        _sunriseSunsetServiceMock.Setup(service => service.ProcessSunriseSunset("jsonData")).Returns(new SunriseSunset());
+        _sunriseSunsetServiceMock.Setup(service => service.AddSunriseSunsetToDb(It.IsAny<SunriseSunset>())).Verifiable();
+        _sunriseSunsetServiceMock.Setup(service => service.ProcessSolarWatch("jsonData")).Returns(new SolarWatch());
+
+        // Act
+        var res = await _solarWatchController.GetSolarWatch(cityName, date);
+
+        // Assert
+        Assert.IsInstanceOf(typeof(OkObjectResult), res.Result);
+    }
+
+    [Test]
+    public async Task GetSolarWatch_ShouldReturnBadRequest_WhenCityServiceThrowsException()
+    {
+        // Arrange
+        var cityName = "TestCity";
+        var date = DateTime.UtcNow;
+        _sunriseSunsetServiceMock.Setup(service => service.GetByCityNameAndDate(cityName, date)).Returns((SunriseSunset)null!);
+        _cityServiceMock.Setup(service => service.GetByName(cityName)).Returns((City)null!);
+        _cityServiceMock.Setup(service => service.GetOpenWeatherMapApiDataAsync(cityName)).ReturnsAsync("jsonData");
+        _cityServiceMock.Setup(service => service.ProcessCityCoordinates("jsonData")).Throws(new Exception());
+
+        // Act
+        var res = await _solarWatchController.GetSolarWatch(cityName, date);
+
+        // Assert
+        Assert.IsInstanceOf(typeof(BadRequestObjectResult), res.Result);
+    }
+
+    [Test]
+    public async Task GetSolarWatch_ShouldReturnBadRequest_WhenExceptionInAddCityToDbAfterFetchingCoordinates()
+    {
+        // Arrange
+        var cityName = "TestCity";
+        var date = DateTime.UtcNow;
+        var coordinates = new Coordinates();
+        _sunriseSunsetServiceMock.Setup(service =>  service.GetByCityNameAndDate(cityName, date)).Returns((SunriseSunset)null!);
+        _cityServiceMock.Setup(service => service.GetByName(cityName)).Returns((City)null!);
+        _cityServiceMock.Setup(service => service.GetOpenWeatherMapApiDataAsync(cityName)).ReturnsAsync("jsonData");
+        _cityServiceMock.Setup(service => service.ProcessCityCoordinates("jsonData")).Returns(coordinates);
+        _cityServiceMock.Setup(service => service.ProcessCity("jsonData")).Returns(new City());
+        _cityServiceMock.Setup(service => service.AddCityToDb(It.IsAny<City>())).Throws(new Exception());
+
+        // Act
+        var res = await _solarWatchController.GetSolarWatch(cityName, date);
+
+        // Assert
+        Assert.IsInstanceOf(typeof(BadRequestObjectResult), res.Result);
+    }
+
+    [Test]
+    public async Task GetSolarWatch_ShouldReturnNotFound_WhenGeneralExceptionOccurs()
+    {
+        // Arrange
+        var cityName = "TestCity";
+        var date = DateTime.UtcNow;
+        _sunriseSunsetServiceMock.Setup(service => service.GetByCityNameAndDate(cityName, date)).Throws(new Exception());
+
+        // Act
+        var res = await _solarWatchController.GetSolarWatch(cityName, date);
+
+        // Assert
+        Assert.IsInstanceOf(typeof(NotFoundObjectResult), res.Result);
+    }
+
+    [Test]
+    public async Task GetSolarWatch_ShouldReturnBadRequest_WhenAddCityToDbFails()
+    {
+        // Arrange
+        var cityName = "TestCity";
+        var date = DateTime.UtcNow;
+        _sunriseSunsetServiceMock.Setup(service => service.GetByCityNameAndDate(cityName, date)).Returns((SunriseSunset)null!);
+        _cityServiceMock.Setup(service => service.GetByName(cityName)).Returns((City)null!);
+        _cityServiceMock.Setup(service => service.GetOpenWeatherMapApiDataAsync(cityName)).ReturnsAsync("jsonData");
+        _cityServiceMock.Setup(service => service.ProcessCityCoordinates("jsonData")).Returns(new Coordinates());
+        _cityServiceMock.Setup(service => service.ProcessCity("jsonData")).Returns(new City());
+        _cityServiceMock.Setup(service => service.AddCityToDb(It.IsAny<City>())).Throws(new Exception());
+
+        // Act
+        var res = await _solarWatchController.GetSolarWatch(cityName, date);
+
+        // Assert
+        Assert.IsInstanceOf(typeof(BadRequestObjectResult), res.Result);
+    }
+
+    [Test]
+    public async Task GetSolarWatch_ShouldReturnBadRequest_WhenAddSunriseSunsetToDbFails()
+    {
+        // Arrange
+        var cityName = "TestCity";
+        var date = DateTime.UtcNow;
+        var coordinates = new Coordinates();
+        _sunriseSunsetServiceMock.Setup(service => service.GetByCityNameAndDate(cityName, date)).Returns((SunriseSunset)null!);
+        _cityServiceMock.Setup(service => service.GetByName(cityName)).Returns((City)null!);
+        _cityServiceMock.Setup(service => service.GetOpenWeatherMapApiDataAsync(cityName)).ReturnsAsync("jsonData");
+        _cityServiceMock.Setup(service => service.ProcessCityCoordinates("jsonData")).Returns(coordinates);
+        _cityServiceMock.Setup(service => service.ProcessCity("jsonData")).Returns(new City());
+        _sunriseSunsetServiceMock.Setup(service => service.GetSunriseSunsetApiDataAsync(date, coordinates)).ReturnsAsync("jsonData");
+        _sunriseSunsetServiceMock.Setup(service => service.ProcessSunriseSunset("jsonData")).Returns(new SunriseSunset());
+        _sunriseSunsetServiceMock.Setup(service => service.AddSunriseSunsetToDb(It.IsAny<SunriseSunset>())).Throws(new Exception());
+
+        // Act
+        var res = await _solarWatchController.GetSolarWatch(cityName, date);
+
+        // Assert
+        Assert.IsInstanceOf(typeof(BadRequestObjectResult), res.Result);
     }
 }
