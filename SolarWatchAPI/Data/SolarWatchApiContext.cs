c9e309183e237307499bb341489aa67184f979f6
ndrs <szaki1006@gmail.com>
2024-07-12 10:08:34 +0200

    :necktie: chore: extend SolarWatchApiContext.cs to auto set DB

--- SolarWatchAPI/Data/SolarWatchApiContext.cs
+++ SolarWatchAPI/Data/SolarWatchApiContext.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
 using SolarWatchAPI.Model.DataModels;
 
 namespace SolarWatchAPI.Data;
@@ -10,10 +12,25 @@ public class SolarWatchApiContext : IdentityDbContext<IdentityUser, IdentityRole
     public DbSet<City> Cities { get; set; }
     public DbSet<SunriseSunset> SunriseSunsets { get; set; }
     private readonly IConfiguration _configuration;
+    private readonly ILogger<SolarWatchApiContext> _logger;
 
-    public SolarWatchApiContext(DbContextOptions<SolarWatchApiContext> options, IConfiguration configuration) : base(options)
+    public SolarWatchApiContext(DbContextOptions<SolarWatchApiContext> options, IConfiguration configuration, ILogger<SolarWatchApiContext> logger) : base(options)
     {
+        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
         _configuration = configuration;
+        try
+        {
+            if (Database.GetService<IDatabaseCreator>() is RelationalDatabaseCreator databaseCreator)
+            {
+                if (!databaseCreator.CanConnect()) databaseCreator.Create();
+                if (!databaseCreator.HasTables()) databaseCreator.CreateTables();
+            }
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, e.Message);
+            throw new Exception(e.Message);
+        }
     }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
