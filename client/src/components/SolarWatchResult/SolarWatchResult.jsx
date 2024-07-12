82699576c78cff4b6c9ac40703b837eac8227572
ndrs <szaki1006@gmail.com>
2024-07-12 09:45:20 +0200

    :necktie: chore: repair SolarWatch page to works well

--- client/src/components/SolarWatchResult/SolarWatchResult.jsx
+++ client/src/components/SolarWatchResult/SolarWatchResult.jsx
@@ -3,11 +3,14 @@ import React from "react";
 
 export default function SolarWatchResult(props) {
     const {
-
+        solarWatch
     } = props
    
     return (
         <>
+            <p>City: {solarWatch.city}</p>             
+            <p>Date: {solarWatch.date.slice(0, 10)}</p>
+            <p>Sunrise: {solarWatch.sunrise} - Sunset: {solarWatch.sunset}</p>
         </>
     )
 
