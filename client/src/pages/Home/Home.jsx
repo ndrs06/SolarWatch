e75e2f41fae1cffd775c537ff59a3884290c098a
ndrs <szaki1006@gmail.com>
2024-07-12 09:40:56 +0200

    :fire: chore: remove navbar from pages (it is in layout)

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,13 +1,10 @@
 import './Home.scss';
 import React, { useState } from 'react';
 
-import Navbar from "../../components/Navbar/Navbar.jsx";
-
 export default function Home() {    
     return (
         <>
             <h1>🏠</h1>
-            <Navbar/>
         </>
     );
 }
