4d9caa9c2bb7f723d349334cce3a2acd7e94854e
ndrs <szaki1006@gmail.com>
2024-09-04 10:54:28 +0200

    :white_check_mark: test: seed data added into integration test factory

--- SolarWatchAPIIntegrationTest/SolarWatchAPIIntegrationTest/SolarWatchWebApplicationFactory.cs
+++ SolarWatchAPIIntegrationTest/SolarWatchAPIIntegrationTest/SolarWatchWebApplicationFactory.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using SolarWatchAPI.Data;
+using SolarWatchAPI.Model.DataModels;
 
 namespace SolarWatchAPIIntegrationTest;
 
@@ -32,6 +33,50 @@ public class SolarWatchWebApplicationFactory : WebApplicationFactory<Program>
             var solarContext = scope.ServiceProvider.GetRequiredService<SolarWatchApiContext>();
             solarContext.Database.EnsureDeleted();
             solarContext.Database.EnsureCreated();
+            
+            SeedData(solarContext);
         });
+    }
+    
+    private void SeedData(SolarWatchApiContext context)
+    {
+        var city1 = new City
+        {
+            Name = "Miskolc",
+            State = "Borsod-Abaúj-Zemplén",
+            Country = "Hungary",
+            Lat = 48.1030,
+            Lon = 20.7789
+        };
+
+        var city2 = new City
+        {
+            Name = "Zamárdi",
+            State = "Somogy",
+            Country = "Hungary",
+            Lat = 46.8820,
+            Lon = 17.9400
+        };
+        
+        var sunriseSunset1 = new SunriseSunset
+        {
+            CityName = "Miskolc",
+            Date = DateTime.UtcNow.Date,
+            Sunrise = new TimeOnly(6, 0),
+            Sunset = new TimeOnly(18, 0)
+        };
+
+        var sunriseSunset2 = new SunriseSunset
+        {
+            CityName = "Zamárdi",
+            Date = DateTime.UtcNow.Date,
+            Sunrise = new TimeOnly(6, 30),
+            Sunset = new TimeOnly(18, 30)
+        };
+        
+        context.Cities.AddRange(city1, city2);
+        context.SunriseSunsets.AddRange(sunriseSunset1, sunriseSunset2);
+        
+        context.SaveChanges();
     }
 }
