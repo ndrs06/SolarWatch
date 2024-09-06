c6290f389d372712ef8b831c7e729003718cba1b
ndrs <szaki1006@gmail.com>
2024-09-06 13:24:07 +0200

    :lipstick: style: removed redundant div from Footer.jsx

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -5,15 +5,13 @@ import { Link } from 'react-router-dom';
 export default function Footer() {
     return (
         <footer className="footer">
-            <div>
-                <div>                    
-                    <Link to="#">About</Link>
-                    <Link to="#">Privacy Policy</Link>
-                    <Link to="#">Licensing</Link>
-                    <Link to="#">Contact</Link>
-                </div>
-                <center><p>######</p></center>
+            <div>                    
+                <Link to="#">About</Link>
+                <Link to="#">Privacy Policy</Link>
+                <Link to="#">Licensing</Link>
+                <Link to="#">Contact</Link>
             </div>
+            <center><p>######</p></center>
         </footer>
     );
 }
