3c7e377973f5d69d9c7e254e625d4c921db8ee1b
ndrs <szaki1006@gmail.com>
2024-09-06 13:24:47 +0200

    :lipstick: style: insert outlets of Layout.jsx into div

--- client/src/pages/Layout/Layout.jsx
+++ client/src/pages/Layout/Layout.jsx
@@ -1,5 +1,5 @@
 import "./Layout.scss"
-import React, {useEffect} from "react";
+import React from "react";
 import Navbar from "../../components/Navbar/Navbar.jsx";
 import Footer from "../../components/Footer/Footer.jsx";
 import {Outlet} from "react-router-dom";
@@ -9,10 +9,12 @@ export default function Layout() {
     return (
         <div className="layout">
             <div className="layout-header-container">
-                <div className="layout-header"></div>
-                <Navbar className="layout-navbar"/>
+                <div className="layout-header"><h1>Solar Watch</h1></div>
+                <Navbar />
             </div>
-            <Outlet className="layout-outlet"/>
+            <div  className="layout-outlet">
+                <Outlet />                
+            </div>
             <Footer className="layout-footer"/>
         </div>
     );
