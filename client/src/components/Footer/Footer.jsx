c417ce5a78a448c0d59d7ae2708e80daadd154c8
ndrs <szaki1006@gmail.com>
2024-09-06 09:55:43 +0200

    :lipstick: style: added a basic footer to layout

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,9 +1,19 @@
 import './Footer.scss';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Footer() {
     return (
         <footer className="footer">
+            <div>
+                <div>                    
+                    <Link to="#">About</Link>
+                    <Link to="#">Privacy Policy</Link>
+                    <Link to="#">Licensing</Link>
+                    <Link to="#">Contact</Link>
+                </div>
+                <center><p>######</p></center>
+            </div>
         </footer>
     );
 }
