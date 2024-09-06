c1342eae889822dd99f0a4786331c21ee664c133
ndrs <szaki1006@gmail.com>
2024-09-06 13:23:16 +0200

    :lipstick: style: insert content of SignUpForm.jsx into div

--- client/src/components/SignUpForm/SignUpForm.jsx
+++ client/src/components/SignUpForm/SignUpForm.jsx
@@ -20,50 +20,56 @@ export default function SignInForm(props) {
     }
 
     return (
-        <form onSubmit={onSubmit}>
-            <div>
-                <label htmlFor="email-sign-up" id="form1" type="email"/>
-                <input
-                    value={user.email}
-                    onChange={e => setUser({...user, email: e.target.value})}
-                    placeholder="Email"
-                    type="email"
-                    name="email"
-                    id="email-sign-up"
-                />
-            </div>
-            <div>
-                <label htmlFor="username-sign-up" id="form1" type="text"/>
-                <input
-                    value={user.username}
-                    onChange={e => setUser({...user, username: e.target.value})}
-                    placeholder="Username"
-                    type="text"
-                    name="username"
-                    id="username-sign-up"
-                />
-            </div>
-            <div>
-                <label htmlFor="password-sign-up" id="form1" type="password"/>
-                <input
-                    value={user.password}
-                    onChange={e => setUser({...user, password: e.target.value})}
-                    placeholder="Password"
-                    type="password"
-                    name="password"
-                    id="password-sign-up"
-                />
-            </div>
-            <div>
-                <Link to="/sign-in">
-                    <p>SignIn</p>
-                </Link>
-            </div>
-
-            <button type="submit">SignUp</button>
-            <button type="button" onClick={onCancel}>Cancel</button>
-
-        </form>
+        <div className="solar-watch-request-form-container">            
+            <form onSubmit={onSubmit}>
+                
+                <div>
+                    <label htmlFor="email-sign-up" id="form1" type="email"/>
+                    <input
+                        value={user.email}
+                        onChange={e => setUser({...user, email: e.target.value})}
+                        placeholder="Email"
+                        type="email"
+                        name="email"
+                        id="email-sign-up"
+                    />
+                </div>
+                
+                <div>
+                    <label htmlFor="username-sign-up" id="form1" type="text"/>
+                    <input
+                        value={user.username}
+                        onChange={e => setUser({...user, username: e.target.value})}
+                        placeholder="Username"
+                        type="text"
+                        name="username"
+                        id="username-sign-up"
+                    />
+                </div>
+                
+                <div>
+                    <label htmlFor="password-sign-up" id="form1" type="password"/>
+                    <input
+                        value={user.password}
+                        onChange={e => setUser({...user, password: e.target.value})}
+                        placeholder="Password"
+                        type="password"
+                        name="password"
+                        id="password-sign-up"
+                    />
+                </div>
+                
+                <div className="form-buttons">                      
+                    <button type="submit">SignUp</button>
+                    <button type="button" onClick={onCancel}>Cancel</button>
+                </div>
+                
+    
+            </form>
+                    <Link to="/sign-in">
+                        <p>SignIn</p>
+                    </Link>
+        </div>
     );
 
 }
